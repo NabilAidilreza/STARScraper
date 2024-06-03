6d1733fec3219c763eaa1c16d639801a2fe460be
Camk <Nabil3N22@gmail.com>
2024-06-03 23:57:59 +0800

    Improved code readability / redundancy
    Fixed bug if file names got error for lunch table creation

--- scripts/ntu_compare_timetables.py
+++ scripts/ntu_compare_timetables.py
@@ -1,104 +1,84 @@
 import os
 import re
+from datetime import datetime
 from .ntu_extract_timetable import generate_timeline, create_timetable_list
 from rich.console import Console
 from rich.theme import Theme
 from rich.table import Table
+from time import sleep
 # from prettytable import *
+
 
 #! FILE THAT MANAGES LOCAL TABLE COMPARISON #
 
-# Clean venue string via regex #
-def check_venue(char):
+#? Helper functions #
+def check_venue(venue_str):
     pattern = r'\((.*?)\)'
-    common_term = re.search(pattern, char)
-    if common_term != None:
-        venue = common_term.group(0)
-        if "The " in venue:
-            venue = venue.replace("The ", "")
-    else:
-        pattern = r'^.*?-'
-        common_term = re.search(pattern, char)
-        if common_term != None:
-            venue = "(" + common_term.group(0)[:-1] + ")"
-        else:
-            venue = '-'
-        if "LHS" in char:
-            venue = "(Hive)"
-        else:
-            if "LHN" in char:
-                venue = "(Arc)"
-    return venue
+    match = re.search(pattern, venue_str)
+    if match:
+        venue = match.group(0)
+        return venue.replace("The ", "") if "The " in venue else venue
+    pattern = r'^.*?-'
+    match = re.search(pattern, venue_str)
+    if match:
+        return f"({match.group(0)[:-1]})"
+    if "LHS" in venue_str:
+        venue = "(Hive)"
+    if "LHN" in venue_str:
+        venue = "(Arc)"
+    return "-"
 
 def get_name(file_name):
     return file_name.replace(".html","").split("_")[-1]
+
+#? Error Checkers #
+def check_file_name(file_name):
+    if "_" not in file_name:
+        return "Incorrect format. (No underscore in name)"
+    if ".html" not in file_name:
+        return "Not a HTML file."
+    try:
+        with open(file_name):
+            pass
+        return ""
+    except FileNotFoundError:
+        return "File not found."
+    
+def validate_week_number(wk_num):
+    try:
+        wk_num = int(wk_num)
+        if wk_num > 13:
+            return "Week number more than 13."
+        if wk_num == 0:
+            return "Zero value."
+        return ""
+    except ValueError:
+        return "Not a number."
+    
+def validate_date(string):
+    try:
+        datetime.strptime(string, '%d/%M/%Y')
+        return ""
+    except ValueError:
+        return "Use correct format. (DD/MM/YYYY)"
      
 # Creates a txt file to compare schedules #
-def compare_grp_timetables(file_name_array,wk_num,start_date):
-    #? Error Checkers #
-    def check_file_name(file_name):
-        if "_" not in file_name:
-            return "Incorrect format. (No underscore in name)"
-        elif ".html" not in file_name:
-            return "Not a HTML file."
-        else:
-            try:
-                file = open(file_name)
-                file.close()
-                return ""
-            except:
-                return "File not found."
-    def check_wk_num(wk_num):
-        try:
-            wk_num = int(wk_num)
-            if wk_num > 13:
-                return "Week number more than 13."
-            if wk_num == 0:
-                return "Zero value."
-            return ""
-        except:
-            return "Not a number."
-    def check_date(string):
-        try:
-            from datetime import datetime
-            date = datetime.strptime(string, '%d/%M/%Y')
-            return ""
-        except ValueError:
-            return "Use correct format. (DD/MM/YYYY)"
-        
-    #? Main function #
-    #* Python Rich #
+def compare_grp_timetables(file_names,wk_num,start_date):
+    #? Console setup #
     custom_theme = Theme({"success":"bold green","error":"bold red","warning":"bold orange_red1","process":"blue_violet"})
     console = Console(theme=custom_theme,record=True)
 
-    check_two = check_wk_num(wk_num)
-    check_three = check_date(start_date)
-    if check_two != "":
+    #? Check params #
+    week_num_error = validate_week_number(wk_num)
+    date_error = validate_date(start_date)
+    if week_num_error != "":
         console.print("Program exited.",style="error")
-        console.print("Reason: " + check_two,style="warning")
+        console.print("Reason: " + week_num_error,style="warning")
         return
-    if check_three != "":
+    if date_error != "":
         console.print("Program exited.",style="error")
-        console.print("Reason: " + check_three,style="warning")
+        console.print("Reason: " + date_error,style="warning")
         return
-
-    NUMBER_OF_PPL = len(file_name_array)
-    teaching_wk = "Teaching Wk" + str(wk_num)
-    time_periods = {"08":0,
-                    "09":1,
-                    "10":2,
-                    "11":3,
-                    "12":4,
-                    "13":5,
-                    "14":6,
-                    "15":7,
-                    "16":8,
-                    "17":9,
-                    "18":10,
-                    "19":11,
-                    "20":12,
-                    "21":13,
-                    "22":14}
 
     try:
         console.print("Creating timeline...",style="process")
@@ -106,45 +86,42 @@ def compare_grp_timetables(file_name_array,wk_num,start_date):
     except ValueError:
         console.print("[error]Error occurred:[/error]" + "[warning] Unknown date string![/warning]\n[error]Exiting program...[/error]")
         return
-    
-    DATES = {}
-    for i in range(len(timeline)):
-        if i == 0:
-            continue
-        else:
-            DATES[i] = timeline[i-1][0] + " to " + timeline[i-1][-2]
 
+    NUMBER_OF_PPL = len(file_names)
+
+    #? For week indexing #
+    teaching_wk = "Teaching Wk" + str(wk_num)
+    DATES = {i: f"{timeline[i-1][0]} to {timeline[i-1][-2]}" for i in range(1, len(timeline))}
     #? For day indexing #
     dayref = ["Mon","Tue","Wed","Thu","Fri"]
-    # dayref = ["Mon","Tue","Wed","Thu","Fri","Sat"]
-    #? For printing #
     DAYS = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"]
-    # DAYS = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"]
-    PERIODS = ["0830to0920", "0930to1020", "1030to1120", "1130to1220", "1230to1320", "1330to1420", "1430to1520", "1530to1620", "1630to1720", "1730to1820","1830to1920","1930to2020","2030to2120","2130to2220"]
-    lst = []
+    # For period index
+    time_periods = {str(i).zfill(2): idx for idx, i in enumerate(range(8, 23))}
+    #? For period printing #
+    PERIODS = [f"{i:02}30to{(i+1):02}20" for i in range(8, 22)]
+    timetables = []
+    errorList = []
 
     # #! TEST CASE #
-    # file_name_array[2] = "FAKE_FILE.html"
-    # file_name_array[3] = "YIKES_html"
+    # file_names[2] = "FAKE_FILE.html"
+    # file_names[3] = "YIKES_html"
     # #! --------- #
 
     console.print("Reading files...",style="process")
-    # Extract all tables #
-    errorList = []
-    for file in file_name_array:
+
+    for file in file_names:
         try:
-            temp = create_timetable_list(file)
-            temp.pop(0)
-            lst.append(temp)
+            timetable = create_timetable_list(file)[1:]
+            timetables.append(timetable)
             console.print("File: " + file + " -> [success]success![/success]")
         except Exception:
             console.print("File: " + file + " -> [error]error![/error]")
             errorList.append(file)
-    if len(errorList) != 0:
-        for errorFile in errorList:
-            file_name_array.remove(errorFile)
-            console.print("File: " + errorFile + " removed from stack.",style="warning")
-            console.print("Reason: " + check_file_name(errorFile),style="warning")
+    
+    for errorFile in errorList:
+        file_names.remove(errorFile)
+        console.print("File: " + errorFile + " removed from stack.",style="warning")
+        console.print("Reason: " + check_file_name(errorFile),style="warning")
         
     # Set up day tables #
     MONDAY = [["" for _ in range(14)] for _ in range(NUMBER_OF_PPL)]
@@ -152,16 +129,14 @@ def compare_grp_timetables(file_name_array,wk_num,start_date):
     WEDNESDAY = [["" for _ in range(14)] for _ in range(NUMBER_OF_PPL)]
     THURSDAY = [["" for _ in range(14)] for _ in range(NUMBER_OF_PPL)]
     FRIDAY = [["" for _ in range(14)] for _ in range(NUMBER_OF_PPL)]
-    # SATURDAY = [["" for _ in range(14)] for _ in range(NUMBER_OF_PPL)]
-    # WEEK = [MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY]
-
     WEEK = [MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY]
 
     console.print("Sorting events...",style="process")
+
     #? Sorting data #
-    for i in range(len(lst)):
+    for i in range(len(timetables)):
         #? lst[i] => person items
-        for week in lst[i]:
+        for week in timetables[i]:
             for item in week:
                 # Display formatting #
                 result = item[0] + " " + (item[9][:3] if item[9] == "Lec/Stu" else item[9]) + " " + check_venue(item[13])
@@ -185,60 +160,57 @@ def compare_grp_timetables(file_name_array,wk_num,start_date):
                                 start_index += 1
                         else:
                             WEEK[day_index][i][start_index] = result
+
+    console.print("Creating chart...",style="process")
+    console.print(f"\n\nTeaching Week {str(wk_num)} -> {DATES[wk_num]}")
     #? Lunch Logic #
     FREE = [{DAYS[_]:{}} for _ in range(len(DAYS))]
-    
-    console.print("Creating chart...",style="process")
-    #? Create chart #
 
-    console.print(f"\n\nTeaching Week {str(wk_num)} -> {DATES[wk_num]}")
-    def setupTable(day):
-        table = Table(title=f"{day[0] + day[1:].lower()}")
-        lst = []
+    def create_day_table(day):
+        table = Table(title=f"{day.capitalize()}")
         table.add_column("Period",style="orange_red1",justify="center")
-        for name in file_name_array:
-            col_name = get_name(name)
-            table.add_column(col_name,style="cyan",justify="center")
+        for name in file_names:
+            table.add_column(get_name(name),style="cyan",justify="center")
         return table
+    
     for i in range(len(WEEK)):
-        table = setupTable(DAYS[i])
+        table = create_day_table(DAYS[i])
         transposed_data = list(map(list, zip(*WEEK[i])))
         for m in range(len(transposed_data)):
             row = transposed_data[m]
-            FREE[i][DAYS[i]][PERIODS[m] if m in [3,4,5] else ""] = [get_name(file_name_array[i]) for i in range(len(row)) if row[i] == ""]
-            row.insert(0,PERIODS[m])
             if len(errorList) != 0:
                 row = row[:-len(errorList)]
+            FREE[i][DAYS[i]][PERIODS[m] if m in [3,4,5] else ""] = [get_name(file_names[j]) for j in range(len(row)) if row[j] == ""]
+            row.insert(0,PERIODS[m])
             table.add_row(*row)
         console.print(table)
-    def setupLunchTable():
-        table = Table(title="Possible Weekly Lunch Timings")
-        table.add_column("Day",style="green",justify="center")
-        table.add_column("Period",style="orange_red1",justify="center")
-        table.add_column("Names",style="cyan")
-        return table
+        sleep(0.5)
 
-    lunch_table = setupLunchTable()
-    temp = []
+    #? Lunch Table Creation #
+    lunch_table = Table(title="Possible Weekly Lunch Timings")
+    lunch_table.add_column("Day",style="green",justify="center")
+    lunch_table.add_column("Period",style="orange_red1",justify="center")
+    lunch_table.add_column("Names",style="cyan")
+
+
+    free_timings = []
     for j in range(len(FREE)):
         day = FREE[j][DAYS[j]]
-        day_temp = []
-        for period in day.keys():
-            day_temp.append(["",period,",".join(day[period])])
-        if day_temp[0][1] == "":
-            day_temp.pop(0)
+        day_temp = [["",period,",".join(day[period])] for period in day.keys()]
+        day_temp.pop(0) if day_temp[0][1] == "" else day_temp
         day_temp[0][0] = dayref[j]
-        temp.append(day_temp)
-    for row in temp:
+        free_timings.append(day_temp)
+    for row in free_timings:
         for col in row:
             lunch_table.add_row(*col)
     console.print(lunch_table)
+
     if not os.path.exists("comparison_tables"):
         os.makedirs("comparison_tables")
     file_name = f"comparison_tables\\WEEK_" + str(wk_num) + "_TABLE.txt"
+    console.save_text(file_name)
     final_dir = os.path.abspath(file_name)
     console.print(f"[success]Txt file has been created for reference.[/success] \n[warning]File is saved here:[/warning] {final_dir}",style="bold yellow")
-    console.save_text(file_name)
     return file_name
 
 ### REFERENCES ###
@@ -261,7 +233,7 @@ def compare_grp_timetables(file_name_array,wk_num,start_date):
 
     # # Print and create txt file (BASIC TEXT)[REDACTED] #
     # lst = []
-    # for name in file_name_array:
+    # for name in file_names:
     #     name = name.split("_")[1]
     #     lst.append(name)
     # def create_pretty_table():
